d216d03832f034875b046127288f63406c0a8521
Andres Moreno <andresmorenojf@gmail.com>
2024-01-17 21:13:55 -0500

    added ITodo interface

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,31 @@ const input = document.getElementById('todoInput') as HTMLInputElement;
 const form = document.querySelector('form')!;
 const list = document.getElementById('todoList')!;
 
-const hanldeSubmit = (event: SubmitEvent) => {
-	event.preventDefault();
-	const newToDo = input.value;
+interface ITodo {
+	text: string;
+	completed: boolean;
+}
+
+const todos: ITodo[] = []
+
+const createTodo = (todo: ITodo) => {
 	const newLi = document.createElement('li');
 	const checkbox = document.createElement('input');
 	checkbox.type = 'checkbox';
 	
-	newLi.append(newToDo);
+	newLi.append(todo.text);
 	newLi.append(checkbox)
 	list.append(newLi);
+}
+
+const hanldeSubmit = (event: SubmitEvent) => {
+	event.preventDefault();
+	const newToDo: ITodo = {
+		text: input.value,
+		completed: false
+	};
+	createTodo(newToDo)
+	todos.push(newToDo)
 
 	input.value = ''
 }
