7b174622357e6c1230996e8e609aee610f4b9948
Andres Moreno <andresmorenojf@gmail.com>
2024-01-17 20:53:47 -0500

    Adding a form

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 const btn = document.getElementById('btn')!; // non-null-expression (!)
 const input = document.getElementById('todoInput') as HTMLInputElement;
+const form = document.querySelector('form')!;
+
+const hanldeSubmit = (event: SubmitEvent) => {
+	event.preventDefault();
+	console.log('Submitted');
+	
+}
+
+form.addEventListener('submit', hanldeSubmit)
 
 // (<HTMLInputElement>input).value // Type Assertions
 
-btn.addEventListener('click', () => {
-	alert(input.value);
-	input.value = ''
-})
+// btn.addEventListener('click', () => {
+// 	alert(input.value);
+// 	input.value = ''
+// })
 
 
 // let mystery: unknown = "Hello world!!!" 
