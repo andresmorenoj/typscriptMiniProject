59a7772b7270d9c15d614d39ca492c124bf6a379
Andres Moreno <andresmorenojf@gmail.com>
2024-01-17 20:45:55 -0500

    more about type assertions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-// const btn = document.getElementById('btn')!; // non-null-expression (!)
+const btn = document.getElementById('btn')!; // non-null-expression (!)
+const input = document.getElementById('todoInput') as HTMLInputElement;
 
-// btn.addEventListener('click', () => alert('click'))
+// (<HTMLInputElement>input).value // Type Assertions
+
+btn.addEventListener('click', () => {
+	alert(input.value);
+	input.value = ''
+})
 
 
-let mystery: unknown = "Hello world!!!" 
+// let mystery: unknown = "Hello world!!!" 
 
-const numChars = (mystery as string).length // Type Assertions (as)
+// const numChars = (mystery as string).length // Type Assertions (as)
+
