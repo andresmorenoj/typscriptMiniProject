35ad9c2dc0a2bbfaa0c8092d9c6c2bd35fa88b52
Andres Moreno <andresmorenojf@gmail.com>
2024-01-17 19:57:14 -0500

    Type Assertions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-const btn = document.getElementById('btn')!; // non-null-expression (!)
+// const btn = document.getElementById('btn')!; // non-null-expression (!)
 
-btn.addEventListener('click', () => alert('click'))
+// btn.addEventListener('click', () => alert('click'))
+
+
+let mystery: unknown = "Hello world!!!" 
+
+const numChars = (mystery as string).length // Type Assertions (as)
